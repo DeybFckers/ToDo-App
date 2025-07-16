581997d322a32bdae536d3f9f723cec02edc86b8
Deybfckers <pucklord322@gmail.com>
2025-07-16 20:09:15 +0800

    Initial commit with ToDo List app

---
+++ lib/LoginPage.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool _secureText = true;
+
+  //test account
+  final String testEmail = "dave@gmail.com";
+  final String testPassword = '123';
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void _login(){
+    if(emailController.text == testEmail &&
+      passwordController.text == testPassword){
+      print('Login successful!');
+    }else{
+      print('Incorrect Credentials!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Centered content
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'To-Do List',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Form(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              //Text-form Size
+                              contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                              prefixIcon: const Icon(Icons.email),
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter your email";
+                              }
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            controller: passwordController,
+                            obscureText: _secureText,
+                            decoration: InputDecoration(
+                              //text-form size
+                              contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 16),
+                              labelText: 'Password',
+                              prefixIcon: const Icon(Icons.key),
+                              suffixIcon: passwordController.text.isNotEmpty
+                                  ? IconButton(
+                                icon: Icon(_secureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    _secureText = !_secureText;
+                                  });
+                                },
+                              )
+                                  : null,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter your password";
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              setState(() {});
+                            },
+                          ),
+                          const SizedBox(height: 20.0),
+                          ElevatedButton(
+                            onPressed: () {
+                              _login();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green[500],
+                              fixedSize: Size(415, 55),
+                            ),
+                            child: const Text('Login',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                          const SizedBox(height: 20),
+                          GestureDetector(
+                            onTap: () {},
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Bottom button
+          Positioned(
+            bottom: 20,
+            left: 15,
+            right: 15,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                fixedSize: const Size(415, 55),
+                side: const BorderSide(color: Colors.green, width: 2),
+              ),
+              child: Text(
+                'Create new account',
+                style: TextStyle(color: Colors.green[500]),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_list/LoginPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title:'To-Do List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
