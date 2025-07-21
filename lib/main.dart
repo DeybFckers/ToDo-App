59009bb4997d02e16e33434a6b338300cdaef343
Deybfckers <pucklord322@gmail.com>
2025-07-21 11:19:30 +0800

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_list/LoginPage.dart';
-import 'package:todo_list/SignupPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
